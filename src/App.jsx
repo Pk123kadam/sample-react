818adaa39ae8742a7212b2b6254c6d282d915025
Junaid Khan <junaid@nimapinfotech.com>
2023-08-04 09:16:27 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <>sampe</>;
+  const [state, setstate] = useState(10);
+  useEffect(() => {
+    if (state < 1) {
+      setstate(0);
+    }
+    let myinterval = setInterval(() => {
+      setstate(state - 1);
+    }, 1000);
+    return () => {
+      clearInterval(myinterval);
+    };
+  }, [state]);
+  return <>count {state} </>;
 }
 
 export default App;
